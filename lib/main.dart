0ddc8716f4996842ee03643297ed9d7f5788e41a
Alexandre Soares <allexandresoares28@gmail.com>
2022-02-19 03:01:52 -0300

    Praticando

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,24 @@ void main() {
 class MeuApp extends StatelessWidget {
   const MeuApp({Key? key}) : super(key: key);
 
+  static const _title = Text('My Card');
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.deepOrange,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            width: 100, //Largura.
+            height: 100, //Altura.
+            color: Colors.white,
+            margin: EdgeInsets.all(20.0), // Espaçamento externo.
+            padding: EdgeInsets.all(20.0), // Espaçamento interno.
+            child: Text('Alexandre'),
+          ),
+        ),
       ),
     );
   }
