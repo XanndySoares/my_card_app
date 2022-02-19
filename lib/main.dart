7ca46c4aa16dade2e982e2e40473f5adcb6df82a
Alexandre Soares <allexandresoares28@gmail.com>
2022-02-19 12:35:43 -0300

    Praticando

--- lib/main.dart
+++ lib/main.dart
@@ -16,13 +16,30 @@ class MeuApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.deepOrange,
         body: SafeArea(
-          child: Container(
-            width: 100, //Largura.
-            height: 100, //Altura.
-            color: Colors.white,
-            margin: EdgeInsets.all(20.0), // Espaçamento externo.
-            padding: EdgeInsets.all(20.0), // Espaçamento interno.
-            child: Text('Alexandre'),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                width: 100.0, //Largura.
+                height: 100, //Altura.
+                color: Colors.white,
+                child: Text('Container 1'),
+              ),
+              SizedBox(height: 20),
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.blue,
+                child: Text('Container 2'),
+              ),
+              SizedBox(height: 20),
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.yellow,
+                child: Text('Container 3'),
+              ),
+            ],
           ),
         ),
       ),
