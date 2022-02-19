693463256032ee3ff14e80f2702694a23b582833
Alexandre Soares <allexandresoares28@gmail.com>
2022-02-19 01:42:24 -0300

    Praticando

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MeuApp());
+}
+
+class MeuApp extends StatelessWidget {
+  const MeuApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.deepOrange,
+        body: Container(),
+      ),
+    );
+  }
+}
