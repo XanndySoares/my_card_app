376a8674a89100117f5c36cdb066a0027b074949
Alexandre Soares <allexandresoares28@gmail.com>
2022-02-24 00:57:54 -0300

    Praticando

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,6 @@ void main() {
 class MeuApp extends StatelessWidget {
   const MeuApp({Key? key}) : super(key: key);
 
-  static const _title = Text('My Card');
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,27 +15,10 @@ class MeuApp extends StatelessWidget {
         backgroundColor: Colors.deepOrange,
         body: SafeArea(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Container(
-                width: 100.0, //Largura.
-                height: 100, //Altura.
-                color: Colors.white,
-                child: Text('Container 1'),
-              ),
-              SizedBox(height: 20),
-              Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.blue,
-                child: Text('Container 2'),
-              ),
-              SizedBox(height: 20),
-              Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.yellow,
-                child: Text('Container 3'),
+              CircleAvatar(
+                backgroundImage: AssetImage('images/FotoTeste.jpg'),
+                radius: 50,
               ),
             ],
           ),
