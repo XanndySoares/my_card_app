280ea4106ff4567491cbfa76721cb9d7140f8606
Alexandre Soares <allexandresoares28@gmail.com>
2022-02-25 18:05:07 -0300

    Aula 24/02/2022

--- lib/main.dart
+++ lib/main.dart
@@ -4,90 +4,87 @@ void main() {
   runApp(MeuApp());
 }
 
-class MeuApp extends StatelessWidget {
+class MeuApp extends StatefulWidget {
   const MeuApp({Key? key}) : super(key: key);
 
   @override
+  State<MeuApp> createState() => _MeuAppState();
+}
+
+class _MeuAppState extends State<MeuApp> {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.red[900],
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                backgroundImage: AssetImage('images/alexandre.JPG'),
-                radius: 50.0,
-              ),
-              Text(
-                'Alexandre Soares',
-                style: TextStyle(
-                  fontSize: 40.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Pacifico',
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: Colors.cyan[900],
+          body: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: NetworkImage(
+                      'https://itforum.com.br/wp-content/uploads/2015/07/linux-1.jpg'),
                 ),
-              ),
-              Text(
-                'DESENVOLVEDOR FLUTTER',
-                style: TextStyle(
-                  fontFamily: 'Source Sans Pro',
-                  color: Colors.white,
-                  fontSize: 20,
-                  letterSpacing: 2.5,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Divider(
-                color: Colors.white,
-              ),
-              Card(
-                margin: EdgeInsets.symmetric(
-                  vertical: 10.0,
-                  horizontal: 25.0,
-                ),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.phone,
-                    size: 25,
-                    color: Colors.red[900],
-                  ),
-                  title: Text(
-                    '+55 94 99234-5390',
-                    style: TextStyle(
-                        fontSize: 20.0,
-                        fontFamily: 'Source Sans Pro',
-                        color: Colors.red[900]),
+                Text(
+                  'Alexandre Soares',
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "Pacifico",
                   ),
                 ),
-              ),
-              Card(
-                margin: EdgeInsets.symmetric(
-                  vertical: 10.0,
-                  horizontal: 25.0,
-                ),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.email,
-                    size: 25,
-                    color: Colors.red[900],
+                Text(
+                  'DESENVOLVEDOR FLUTTER',
+                  style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    color: Colors.white,
+                    fontSize: 20,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
                   ),
-                  title: Text(
-                    'allexandresoares28@gmail.com',
-                    style: TextStyle(
-                      color: Colors.red[900],
-                      fontFamily: 'Source Sans Pro',
-                      fontSize: 20.0,
+                ),
+                Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      size: 25,
+                      color: Colors.red,
+                    ),
+                    title: Text(
+                      '+55 94 99234-5390',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.red,
+                          fontFamily: 'Source Sans Pro'),
                     ),
                   ),
                 ),
-              ),
-            ],
+                Card(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 25.0,
+                  ),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      size: 25,
+                      color: Colors.red,
+                    ),
+                    title: Text('allexandresoares28@gmail.com',
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            fontFamily: 'Source Sans Pro',
+                            color: Colors.red)),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
